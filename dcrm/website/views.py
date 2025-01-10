071437d6ec72c9f8896ffa9b3bec7536b7c42d55
donnobani <55165390+donnobani@users.noreply.github.com>
2025-01-09 20:32:44 -0500

    added register form

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
-# Create your views here.
 def home(request):
     # Check to see if logging in
     if request.method == 'POST':
@@ -25,3 +25,20 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out.")
     return redirect('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and log in
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have succesfully registered! Welcome!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
